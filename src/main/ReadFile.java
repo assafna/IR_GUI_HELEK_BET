3dbc43f2e51eb81da1b4e745259c4eb68c2c1b8a
amitmag <amitmag@post.bgu.ac.il>
2017-12-29 13:07:23 +0200

    readfile dates

--- src/main/ReadFile.java
+++ src/main/ReadFile.java
@@ -77,25 +77,28 @@ public class ReadFile {
 
                     while (line != null) {
 
-                        //looking for date
-                        if (line.contains("<DATE1>")) {
-                            lineCharArray = line.toCharArray();
-                            int i = 7;
-                            while (lineCharArray[i] == 32)
-                                i++;
-
-                            while (lineCharArray[i] != '<') {
-                                if (lineCharArray[i] == 32 && lineCharArray[i + 1] == '<')
-                                    break;
-                                if (lineCharArray[i] == 32 && lineCharArray[i + 1] == 32) {
-                                    i++;
-                                    continue;
+                        //find date of files that start with LA
+                        if(line.contains("<DATE")) {
+                            if (file.getName().startsWith("LA") && line.contains("<DATE>")) {
+                                line = br.readLine();
+                                line = br.readLine();
+                                lineCharArray = line.toCharArray();
+                                int countComma = 0;
+                                int index = 0;
+                                while (index < lineCharArray.length) {
+                                    if (lineCharArray[index] == ',')
+                                        countComma++;
+                                    if (countComma == 2)
+                                        break;
+                                    else
+                                        date.append(lineCharArray[index]);
+                                    index++;
                                 }
-                                date.append(lineCharArray[i]);
-                                i++;
-
-                            }
+                            } else
+                                //find date for files that start with FB or FT
+                                date = parseDateForFBorFTfiles(line, file.getName());
                         }
+
 
                         //looking for text
                         if (line.contains("<TEXT>")) {
@@ -157,6 +160,55 @@ public class ReadFile {
             e.printStackTrace();
         }
         return stopWords;
+    }
+
+    /**
+     * parse date for files that start with "FB" or "FT"
+     * @param line line to parse
+     * @param fileName name of the file
+     * @return date
+     */
+    private StringBuilder parseDateForFBorFTfiles(String line, String fileName) {
+        StringBuilder date = new StringBuilder();
+
+            char[] lineCharArray;
+
+            //for files that start with "FB"
+            if (fileName.startsWith("FB") && line.contains("<DATE1>")) {
+                lineCharArray = line.toCharArray();
+                int i = 7;
+                while (lineCharArray[i] == 32)
+                    i++;
+
+                while (lineCharArray[i] != '<') {
+                    if (lineCharArray[i] == 32 && lineCharArray[i + 1] == '<')
+                        break;
+                    if (lineCharArray[i] == 32 && lineCharArray[i + 1] == 32) {
+                        i++;
+                        continue;
+                    }
+                    date.append(lineCharArray[i]);
+                    i++;
+
+                }
+                return date;
+            }
+
+            //for files that start with "FT"
+            if (fileName.startsWith("FT") && line.contains("<DATE>")) {
+                lineCharArray = line.toCharArray();
+                date.append(lineCharArray[10]);
+                date.append(lineCharArray[11]);
+                date.append("/");
+                date.append(lineCharArray[8]);
+                date.append(lineCharArray[9]);
+                date.append("/19");
+                date.append(lineCharArray[6]);
+                date.append(lineCharArray[7]);
+
+                return date;
+            }
+        return date;
     }
 
     public int getNumOfDocs() {
